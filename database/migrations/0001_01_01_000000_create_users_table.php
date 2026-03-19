318b1e11edc3b68309df4d6b065a37bb574c4df6
raulcitorm <raulrodmartin@gmail.com>
2026-03-19 09:19:28 +0000

    migraciones

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,10 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->primary();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->text('descripcion')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
