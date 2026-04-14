92f55974bb3e0633263828f248876ac6354e7ce1
josejr3 <gregorio.jr2@gmail.com>
2026-04-14 09:47:51 +0100

    fix de la carpeta web

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,4 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create()
+    ->usePublicPath(__DIR__.'/../../web');
